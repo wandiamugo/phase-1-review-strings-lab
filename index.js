ac07947594283c4f53f97694543cfc8496bdef80
Wandia E Mugo <wwandia173@gmail.com>
2022-06-06 13:48:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser= "Wandia Mugo";
+const welcomeMessage ="Welcome to Flatbook, Wandia Mugo!";
+const excitedWelcomeMessage="WELCOME TO FLATBOOK, WANDIA MUGO!"
+const shortGreeting="Welcome, W!"
