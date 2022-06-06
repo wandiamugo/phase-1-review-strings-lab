f735c26cf3cfd0c769be07ac2ed3e82acd94f874
Wandia E Mugo <wwandia173@gmail.com>
2022-06-06 14:00:24 +0300

    commit tree 135bdcd4f021f763f9bf444b21aa27f0b9f46ea0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code in this file!
 const currentUser= "Wandia Mugo";
-const welcomeMessage ="Welcome to Flatbook, Wandia Mugo!";
+const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage="WELCOME TO FLATBOOK, WANDIA MUGO!"
 const shortGreeting="Welcome, W!"
+
